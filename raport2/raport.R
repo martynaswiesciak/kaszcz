6d74e850e7c72b7c2751a7ac4f6b85ff36e4b90c
Martyna <baaalbinka@icloud.com>
2020-02-03 22:17:52 +0100

    transformacja sqrt

--- raport2/raport.R
+++ raport2/raport.R
@@ -16,10 +16,11 @@ abline(v=1960:1985, col=50, lty=3)
 
 dane.test <- window(serie.all,start=c(1985,1)) # dane do predykcji
 
-# AUTOKORELACJA
+# acf i pacf
 acf(serie,100, main = "Funkcja autokorelacji")
 pacf(serie,100, main = "Funkcja częściowej autokorelacji")
 
+#badanie statystyk
 m=apply(matrix(serie,nr=12),2,mean)
 v=apply(matrix(serie,nr=12),2,var)
 plot(m,v,xlab="Średnia roczna", ylab="Wariancja roczna")
@@ -33,3 +34,18 @@ boxplot(serie~floor(time(serie)), xlab = "Lata", ylab="Opady [mm]")
 plot(1960:1984, m, main="Średnia rocznie", xlab='lata', ylab= '')
 plot(1960:1984, v, main="Wariancja rocznie", xlab='lata', ylab='')
 
+# TRANSFORMACJA DANYCH - nakładamy pierwiastek kwadratowy
+sqrtserie <- sqrt(serie)
+acf(sqrtserie,100, main = "Funkcja autokorelacji")
+pacf(sqrtserie,100, main = "Funkcja częściowej autokorelacji")
+
+sqrtm=apply(matrix(sqrtserie,nr=12),2,mean)
+sqrtv=apply(matrix(sqrtserie,nr=12),2,var)
+plot(sqrtm,sqrtv,xlab="Średnia roczna", ylab="Wariancja roczna")
+abline(lm(sqrtv~sqrtm),col=6,lty=3,lwd=2)
+summary(lm(sqrtv~sqrtm))
+
+boxplot(serie~floor(time(sqrtserie)), xlab = "Lata", ylab="Opady [mm]")
+
+plot(1960:1984, sqrtm, main="Średnia rocznie", xlab='lata', ylab= '')
+plot(1960:1984, sqrtv, main="Wariancja rocznie", xlab='lata', ylab='')
