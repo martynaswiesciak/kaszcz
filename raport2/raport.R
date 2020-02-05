08a3c89919c272bb7daafa2d79fe4af9af59facd
Kasia <nigdyja@op.pl>
2020-02-06 00:33:06 +0100

    prognoza i nowe wykresy

--- raport2/raport.R
+++ raport2/raport.R
@@ -4,17 +4,17 @@ graphics.off()
 library(forecast)
 library(tseries)
 
-data <- read.csv('/Users/martynaswiesciak/Desktop/Studia/sem5/KASZCZ/kaszcz/raport2/bangladesz.csv')
+#data <- read.csv('/Users/martynaswiesciak/Desktop/Studia/sem5/KASZCZ/kaszcz/raport2/bangladesz.csv')
 serie.all <- ts(data$rain, start = c(1901, 1), end = c(2015, 12), frequency = 12)
 start(serie.all)
 
-# PODZIAŁ DANYCH NA PRÓBKĘ TESTOWĄ I DO PREDYKCJI
+# PODZIA� DANYCH NA PRÓBKĘ TESTOWĄ I DO PREDYKCJI
 serie.all <- window(serie.all, start=c(1960, 1))
 serie <- window(serie.all,end=c(1984,12)) # dane testowe
 plot(serie, main='Opady w Bangladeszu', xlab='lata', ylab='opady [mm]') 
 abline(v=1960:1985, col=50, lty=3)
 
-dane.test <- window(serie.all,start=c(1985,1)) # dane do predykcji
+dane.test <- window(serie.all,start=c(1985,1), end=c(1990,12)) # dane do predykcji
 
 # acf i pacf
 acf(serie,100, main = "Funkcja autokorelacji")
@@ -38,6 +38,10 @@ plot(1960:1984, v, main="Wariancja rocznie", xlab='lata', ylab='')
 sqrtserie <- sqrt(serie)
 acf(sqrtserie,100, main = "Funkcja autokorelacji")
 pacf(sqrtserie,100, main = "Funkcja częściowej autokorelacji")
+
+plot(sqrtserie, main='', xlab='lata', ylab='') 
+abline(v=1960:1985, col=6, lty=3)
+
 
 sqrtm=apply(matrix(sqrtserie,nr=12),2,mean)
 sqrtv=apply(matrix(sqrtserie,nr=12),2,var)
@@ -90,8 +94,8 @@ summary(mod)
 arima.auto.bic<-auto.arima(sqrtserie, ic="bic")
 summary(arima.auto.bic)
 
-arima.auto<-auto.arima(sqrtserie, ic="aic")
-summary(arima.auto)
+arima.auto.aic<-auto.arima(sqrtserie, ic="aic")
+summary(arima.auto.aic)
 
 # WALIDACJA MODELI
 mod.reszty<-mod$residuals
@@ -110,13 +114,13 @@ acf(arima.auto.bic.reszty^2,50)
 tsdiag(arima.auto.bic,gof.lag=24)
 Box.test(arima.auto.bic.reszty,type="Ljung-Box",lag=2,fitdf=1)
 
-arima.auto.reszty<-arima.auto$residuals
-plot(arima.auto.reszty)
-acf(arima.auto.reszty,50)
-pacf(arima.auto.reszty,50)
-acf(arima.auto.reszty^2,50)
-tsdiag(arima.auto,gof.lag=24)
-Box.test(arima.auto.reszty,type="Ljung-Box",lag=2,fitdf=1)
+arima.auto.aic.reszty<-arima.auto.aic$residuals
+plot(arima.auto.aic.reszty)
+acf(arima.auto.aic.reszty,50)
+pacf(arima.auto.aic.reszty,50)
+acf(arima.auto.aic.reszty^2,50)
+tsdiag(arima.auto.aic.reszty,gof.lag=24)
+Box.test(arima.auto.aic.reszty,type="Ljung-Box",lag=2,fitdf=1)
 
 # RESIDUA - ANALIZA
 qqnorm(mod.reszty)
@@ -133,10 +137,62 @@ hist(arima.auto.bic.reszty,breaks=20,freq=F, main='Histogram', xlab='', ylab='G�
 curve(dnorm(x,mean=mean(arima.auto.bic.reszty),sd=sd(arima.auto.bic.reszty)),col=2,add=T)
 
 
-qqnorm(arima.auto.reszty)
-qqline(arima.auto.reszty,col=2,lwd=2)
+qqnorm(arima.auto.aic.reszty)
+qqline(arima.auto.aic.reszty,col=2,lwd=2)
 
-hist(arima.auto.reszty,breaks=20,freq=F, main='Histogram', xlab='', ylab='Gęstość')
-curve(dnorm(x,mean=mean(arima.auto.reszty),sd=sd(arima.auto.reszty)),col=2,add=T)
+hist(arima.auto.aic.reszty,breaks=20,freq=F, main='Histogram', xlab='', ylab='Gęstość')
+curve(dnorm(x,mean=mean(arima.auto.aic.reszty),sd=sd(arima.auto.aic.reszty)),col=2,add=T)
 
 # PREDYKCJA
+par(mfrow=c(1,1))
+
+prognoza.mod<-forecast(mod,h=6*12)
+plot(prognoza.mod,main="Prognoza modelu nr 1")
+lines(sqrt(dane.test), col="red")
+lines(sqrtserie,col='red')
+accuracy(mod)
+accuracy(mod,sqrt(dane.test))
+
+prognoza.arima.auto.bic<-forecast(arima.auto.bic,h=6*12)
+plot(prognoza.arima.auto.bic,main="Prognoza modelu nr 2")
+lines(sqrt(dane.test), col="red")
+lines(sqrtserie,col='red')
+accuracy(prognoza.arima.auto.bic)
+accuracy(prognoza.arima.auto.bic,sqrt(dane.test))
+
+
+prognoza.arima.auto.aic<-forecast(arima.auto.aic,h=6*12)
+plot(prognoza.arima.auto.aic,main="Prognoza modelu nr 3")
+lines(sqrt(dane.test), col="red")
+lines(sqrtserie,col='red')
+accuracy(prognoza.arima.auto.aic)
+accuracy(prognoza.arima.auto.aic,sqrt(dane.test))
+
+#�rednia szeroko�� przedzia�u ufno�ci
+mean(prognoza.mod$upper[1:72,2]-prognoza.mod$lower[1:72,2])
+mean(prognoza.arima.auto.bic$upper[1:72,2]-prognoza.arima.auto.bic$lower[1:72,2])
+mean(prognoza.arima.auto.aic$upper[1:72,2]-prognoza.arima.auto.aic$lower[1:72,2])
+
+
+# 
+# ultim=c(1984,12)
+# pred=predict(mod,n.ahead=6*12)
+# pr<-ts(c(tail(sqrtserie,1),pred$pred),start=ultim,freq=12)
+# se<-ts(c(0,pred$se),start=ultim,freq=12)
+# 
+# 
+# 
+# #Intervals
+# tl1<-ts((pr-1.96*se)^2,start=ultim,freq=12)
+# tu1<-ts((pr+1.96*se)^2,start=ultim,freq=12)
+# pr1<-ts((pr)^2,start=ultim,freq=12)
+# 
+# ts.plot(serie,tl1,tu1,pr1,lty=c(1,2,2,1),col=c(1,4,4,2),type="l",main="Model 1 ")
+# abline(v=1949+0:13,lty=3,col=4)
+# lines(serie.all)
+
+
+
+
+
+
